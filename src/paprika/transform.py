53c1cea7a3c8c1ac781c2ede38bd7a4246c24506
alearningcurve <will@willwalling.com>
2025-10-31 13:08:48 -0400

    stash: temp change

---
+++ src/paprika/transform.py
@@ -0,0 +1,11 @@
+from src.paprika.parser import Recipe
+import pandas as pd
+
+INPUT_PATH = "data/export.json"
+OUTPUT_PATH = "data/processed_data.json"
+
+df = pd.read_json(INPUT_PATH)
+
+# %%
+
+# %%
